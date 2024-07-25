557f7d8f4a8c349ea09142a69d7303b3177678dd
Tifany <tifsnunes@gmail.com>
2024-07-25 20:01:28 -0300

    active class in keyelement

--- script.js
+++ script.js
@@ -2,10 +2,24 @@ document.body.addEventListener("keyup", (event) => {
   playSound(event.code.toLocaleLowerCase());
 });
 
+document.querySelector(".composer button").addEventListener("click", () => {
+  let song = document.querySelector("#input").value;
+});
+
 function playSound(sound) {
   let audioElement = document.querySelector(`#s_${sound}`);
+  let keyElement = document.querySelector(`div[data-key="${sound}"]`);
 
   if (audioElement) {
+    audioElement.currentTime = 0;
     audioElement.play();
+  }
+
+  if (keyElement) {
+    keyElement.classList.add("active");
+
+    setTimeout(() => {
+      keyElement.classList.remove("active");
+    }, 300);
   }
 }
