446c7c3cfa67a903ec0d15b210e2da41c7f23963
Tifany <tifsnunes@gmail.com>
2024-07-26 12:34:53 -0300

    playComposition array

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ document.body.addEventListener("keyup", (event) => {
 
 document.querySelector(".composer button").addEventListener("click", () => {
   let song = document.querySelector("#input").value;
+
+  if (song !== "") {
+    let songArray = song.split("");
+    playComposition(songArray);
+  }
 });
 
 function playSound(sound) {
@@ -21,5 +26,17 @@ function playSound(sound) {
     setTimeout(() => {
       keyElement.classList.remove("active");
     }, 300);
+  }
+}
+
+function playComposition(songArray) {
+  let wait = 0;
+
+  for (let songItem of songArray) {
+    setTimeout(() => {
+      playSound(`key${songItem}`);
+    }, wait);
+
+    wait += 250;
   }
 }
